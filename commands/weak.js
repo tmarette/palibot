57fabd5535b9df68d41fba9872bc605ecd8ff262
Crousthybalt <thibault.marette@gmail.com>
2018-11-10 01:54:55 +0100

    Bug fix

--- commands/weak.js
+++ commands/weak.js
@@ -72,7 +72,9 @@ module.exports = {
                 ail = ail.concat(ailments[i]).concat('\n');
               }
             }
-
+            if (ail === ""){
+              ail = "None."
+            }
               const deb2= doc.indexOf('<td colspan="2" style="background-color:#3A5766; color:#ffffff; font-weight:bold; font-size:9pt; text-align:center;"><b>Monster Hunter');
               const fin2 =  doc.indexOf('<b>Threat Level');
               var doc_thumb = doc.substring(deb2,fin2);

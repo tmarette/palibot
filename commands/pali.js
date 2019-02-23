190a0c041203629af90c720e7834f8307dba1d0c
Crousthybalt <thibault.marette@gmail.com>
2019-02-23 15:05:14 +0100

    better help

--- commands/pali.js
+++ commands/pali.js
@@ -13,14 +13,15 @@ module.exports = {
   			const embed = new Discord.RichEmbed()
   		  .setTitle(titre)
   		  .setColor("RANDOM")
-  		  .setDescription("I'm here to help you hunt big meownsters ! :3")
+  		  .setDescription("I'm here to help you hunt big meownsters ! :smiley_cat: ")
 
   		  .addField("How to use me.",
-  		    "*```weak <monster>``` makes me display one monster's weaknesses and ailments. \n\n ")
-  		  .addField("What if I can't find the monster ?", "Maybe you typed the monster's name wrong.\n I can understand for instance ```weak yIAN Kut-KU``` but not ```weak yian kut ku``` because of the missing hyphen.\nOtherwise, maybe it's a bug. If you want to see it fixed, please dm /u/yasefumi on reddit :)\n\n", true)
+  		    "```weak <monster>``` makes me display one monster's weaknesses and ailments. \n\n ")
+  		  .addField("What if I can't find the monster ?", "Maybe you typed the monster's name wrong.\n I can understand for instance ```weak yIAN Kut-KU``` but not ```weak yian kut ku``` because of the missing hyphen.\n\n"
+        +"Otherwise, maybe it's a bug. If you want to see it fixed, please pm /u/yasefumi on reddit :)\n\n")
 
   		  .addBlankField(true)
-  		  .addField("What if I was a good bot ?", "You can pet me with the command ```pali pet```", true);
+  		  .addField("What if I was a good bot ?", "You can pet me with the command ```pali pet```");
         embed.setThumbnail('https://ih0.redbubble.net/image.98407351.3167/st%2Csmall%2C215x235-pad%2C210x230%2Cf8f8f8.lite-1u2.jpg');
 
   		  message.channel.send({embed});

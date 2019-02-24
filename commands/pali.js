e6c4e5b27f450b699e76eed527f168127ff24429
Crousthybalt <thibault.marette@gmail.com>
2019-02-24 22:47:12 +0100

    now autocomplete monsters

--- commands/pali.js
+++ commands/pali.js
@@ -9,7 +9,7 @@ module.exports = {
       const msg = message.content.toLowerCase();
 
       if (msg.includes('help')){
-        var titre = "Hi ! I'm the palibot !";
+        var titre = "Nyaa ! I'm the palibot !";
   			const embed = new Discord.RichEmbed()
   		  .setTitle(titre)
   		  .setColor("RANDOM")
@@ -18,17 +18,17 @@ module.exports = {
   		  .addField("How to use me.",
   		    "```weak <monster>```makes me display one monster's weaknesses, ailments, and a\nrandom trivia\n ```info <monster>``` only makes me display its weaknesses and ailments.")
         .addBlankField(true)
-  		  .addField("What if I can't find the monster ?", "Maybe you typed the monster's name wrong.\nI can understand for instance `weak yIAN Kut-KU` but I can't\nunderstand `weak yian kut ku` because of the missing hyphen.\n\n"
+  		  .addField("What if I can't find the meownster ?", "Maybe you typed the meownster's name wrong.\nI can understand for instance `weak yIAN Kut-KU` but I can't\nunderstand `weak yian kut ku` because of the missing hyphen.\n\n"
         +"Otherwise, maybe it's a bug. If you want to see it fixed, please pm [/u/yasefumi](https://www.reddit.com/message/compose/?to=Yasefumi) on reddit :)")
 
   		  .addBlankField(true)
-  		  .addField("What if I was a good bot ?", "You can pet me with the command ```pali pet```");
+  		  .addField("What if I was a good bot ?", "You can pet me with the command ```pali pet`` nyaa`");
         embed.setThumbnail('https://ih0.redbubble.net/image.98407351.3167/st%2Csmall%2C215x235-pad%2C210x230%2Cf8f8f8.lite-1u2.jpg');
         console.log(`${message.guild.name}` + ` (${message.guild.memberCount} users) ${message.author.username} needed help.`)
   		  message.channel.send({embed});
 		  }
       if (msg.includes('pet')){
-        message.channel.send('Thank you master *purr purr*')
+        message.channel.send('Thank you Meowster *purr purr*')
         console.log(`${message.guild.name}` + ` (${message.guild.memberCount} users) ${message.author.username} has pet me.`);
 
       }
--- commands/weak.js
+++ commands/weak.js
@@ -4,7 +4,7 @@ const {elements,ailments } = require('../config.json');
 
 module.exports = {
     name: 'weak',
-    description: 'i give one monster weaknesses',
+    description: 'i give one monster\'s weaknesses and ailments',
     execute(message, args) {
 
       function capitalize(string) {
@@ -33,7 +33,7 @@ module.exports = {
 
         monstre = monstre.slice(0,monstre.length-1)
         prettyname = prettyname.slice(0,prettyname.length-1)
-        const wiki = "https://monsterhunter.fandom.com/wiki/" + monstre //The url we are fetching
+        var wiki = "https://monsterhunter.fandom.com/wiki/" + monstre //The url we are fetching
 
 
 
@@ -93,75 +93,8 @@ module.exports = {
           const end_thumbnail = doc_thumb.indexOf('  	 width=')-1;
           doc_thumb = doc_thumb.substring(begin_thumbnail,end_thumbnail);
 
-          //Finally, let's add one random note.
-          const begin_trivia = doc.indexOf('<h2><span class="mw-headline" id="Notes">Notes</span></h2>');
-          var doc_trivia = doc.substring(begin_trivia);
-          const end_trivia = doc_trivia.indexOf('</p></div>')-6;
-          var doc_trivia = doc_trivia.substring(0,end_trivia);
-          const begin_narrow_trivia = doc_trivia.indexOf("<ul>")+4;
-          doc_trivia = doc_trivia.substring(begin_narrow_trivia);
-          const garbage_tab = doc_trivia.split("<li>");
-          const trivia_tab = []
-          var incrementer = true
-
-          //We have to deal with lists within lists....
-
-          for (var i = 0; i < garbage_tab.length;i++){
-            if (garbage_tab[i].includes("</ul>")){
-              incrementer = true;
-              trivia_tab[trivia_tab.length-1]+="-"+garbage_tab[i]
-            }
-            else if (garbage_tab[i].includes("<ul>")){
-              incrementer = false;
-              trivia_tab.push(garbage_tab[i])
-            }
-            else {
-              if (incrementer){
-                trivia_tab.push(garbage_tab[i])
-              }
-            else {
-                trivia_tab[trivia_tab.length-1]+="-"+garbage_tab[i]
-              }
-            }
-          }
-
-          for (var i = 1; i<trivia_tab.length;i++){
-            if (trivia_tab[i].length <= 0 || trivia_tab[i].length>=1024){
-              trivia_tab.splice(i, 1);
-            }
-          }
-          const trivia = trivia_tab[Math.floor((Math.random())*trivia_tab.length)]
 
 
-
-          //now we cleanup the trivia.
-          const trivia_bits = trivia.split('href=')
-          var final_trivia = trivia_bits[0]
-          for (var i = 1; i<trivia_bits.length;i++){
-            var ugly_trivia = trivia_bits[i]
-            var link = "https://monsterhunter.fandom.com"
-            var begin_link = 1
-            var end_link = ugly_trivia.indexOf("title")
-            const wikilink = ugly_trivia.substring(begin_link,end_link-2).replace(/\(/g, "%28").replace(/\)/g,"%29")
-            link += wikilink
-            begin_link = ugly_trivia.indexOf(">")+1
-            end_link = ugly_trivia.indexOf("<")
-            const link_name = ugly_trivia.substring(begin_link,end_link)
-            var end_of_trivia = ugly_trivia.substring(ugly_trivia.indexOf("</a>")+4)
-            final_trivia += "["+link_name+"]("+link+")"+end_of_trivia
-
-          }
-
-          final_trivia = final_trivia.replace(/<i>/g, "")
-            .replace(/<a/g, "")
-            .replace(/<\/i>/g, "")
-            .replace(/<\/li>/g, '')
-            .replace(/<ul>/g, '')
-            .replace(/<\/ul>/g,'')
-
-            if (final_trivia.includes("class")){
-              final_trivia = ""
-            }
 
 
           //Now creating the embed message
@@ -172,19 +105,36 @@ module.exports = {
             //.setDescription(wiki)
             .addField("Weakness(es) : ", weaknesses, true)
             .addField("Ailment(s) : ",ail, true )
-            if (trivia.length > 0 && trivia.length<1024){
-              embed.addField("Note :",final_trivia)
-            }
+
             if (doc_thumb.includes(".png")){embed.setThumbnail(doc_thumb);} //If there is a fitting image, then it is the thumbnail
 
               console.log("Success."); //Let's put in the logs that the request is a success
               return ({embed}) //Let's return the final message
             }
-        console.log(message.guild.name + ` (${message.guild.memberCount} users)` + " -> "+ prettyname + ` (request by ${message.author.username})`); //Nice logs
-        fetch(wiki)
-            .then(res => res.text())
-            .then(body => message.channel.send(wik(body)))
 
+        var a_trouve = false
+        //If the monster is in the list
+        for (var i=0;i<monster_list.length;i++){
+          if (monstre === monster_list[i]){
+          fetch(wiki)
+              .then(res => res.text())
+              .then(body => message.channel.send(wik(body)))
+              a_trouve = true
+              break;
+            }
           }
-
-      }
+        //Otherwise we look for the first occurence of the monster in the list
+        if (!a_trouve){
+          for (var i=0;i<monster_list.length;i++){
+            if (monster_list[i].includes(monstre)){
+              prettyname=monster_list[i].replace(/_/g," ")
+              wiki = "https://monsterhunter.fandom.com/wiki/" + monster_list[i]
+              fetch(wiki)
+                  .then(res => res.text())
+                  .then(body => message.channel.send(wik(body)))
+              break;
+            }
+          }
+        }
+        console.log(message.guild.name + ` (${message.guild.memberCount} users)` + " => "+ prettyname + ` (request by ${message.author.username})`); //Nice logs
+    }}
